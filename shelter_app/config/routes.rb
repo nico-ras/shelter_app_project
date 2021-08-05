68d8c4fbf58acd53493dd02a5fe30c205667d964
nico <n.farina.cu@gmail.com>
2021-08-05 02:38:36 -0300

    user registration with attr :nickname :admin

--- shelter_app/config/routes.rb
+++ shelter_app/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   
   
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   resources :homes
   resources :shelters do
