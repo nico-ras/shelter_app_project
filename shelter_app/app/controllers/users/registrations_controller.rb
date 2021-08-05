e6df03431cd5704c3831e3ab3c5048d79bbea231
nico <n.farina.cu@gmail.com>
2021-08-05 18:59:53 -0300

    just admin user can create new users

--- shelter_app/app/controllers/users/registrations_controller.rb
+++ shelter_app/app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :require_no_authentication, only: [:cancel ]
 
   # GET /resource/sign_up
   # def new
